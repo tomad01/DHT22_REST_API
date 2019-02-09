bbb1366a429ec0bbf6f8a4baac76245587262405
tomad01 <32391186+tomad01@users.noreply.github.com>
2019-02-09 11:25:36 +0200

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,16 +1,40 @@
 import Adafruit_DHT as dht
+from Adafruit_BMP import BMP085
 from datetime import datetime
 import time,csv
 
 
-def get_data():
-    """this func is returning (temp,hum) as numbers with two decimals casted in strings"""
+def get_dht22_data():
     try:
         h,t = dht.read_retry(dht.DHT22, 4)
-        return ("%.2f"%t,"%.2f"%h)
+        status = "Ok"
     except Exception as er:
-        print str(er)
-        return ("0","0")
+        status = str(er)
+        h = 0
+        t = 0
+    return {'status':status,
+            'temperature':{'value':"%.2f"%t,'unit':'C'},
+            'humidity':   {'value':"%.2f"%h,'unit':'%'}}
+
+
+def get_bmp180_data():
+    try:
+        bmp = BMP085.BMP085()
+        status = "Ok"
+        temp = bmp.read_temperature()
+        pressure = bmp.read_pressure()
+        altitude = bmp.read_altitude()
+    except Exception as er:
+        status = str(er)
+        temp = 0
+        pressure = 0
+        altitude = 0
+    return {'status':status,
+            'temperature':{'value':"%.2f"%temp,'unit':'C'},
+            'pressure':   {'value':"%.2f"%(pressure / 100.0),'unit':'hPa'},
+            'altidude':   {'value':"%.2f"%altitude,'unit':'m'}}
+
+
     
 
     
